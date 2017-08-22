28040124badf29ac97cbe055f84c57f666cba362
smartlz <1009098051@qq.com>
2017-08-22 12:03:17 +0800

    删除df_user的str方法

--- dailyfresh/df_user/models.py
+++ dailyfresh/df_user/models.py
@@ -4,6 +4,4 @@ from django.db import models
 class UserInfo(models.Model):
     username = models.CharField(max_length=20)
     password = models.CharField(max_length=40)
-
-    def __str__(self):
-        return self.username.encode('utf-8')
+    
